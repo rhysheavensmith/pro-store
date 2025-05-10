fa796b31e711b82786b5373e8a25085d17319160
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2025-05-10 16:31:03 +1200

    added eslint ignore rules for prisma

--- db/seed.ts
+++ db/seed.ts
@@ -2,9 +2,16 @@ import sampleData from "@/db/sample-data";
 import { prisma } from "@/db/prisma";
 
 const seed = async () => {
-  await prisma.product.deleteMany();
-  await prisma.product.createMany({ data: sampleData.products });
-  console.log("Database seeded successfully");
+  try {
+    await prisma.product.deleteMany();
+    await prisma.product.createMany({ data: sampleData.products });
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error(`Error seeding database: ${error}`);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 seed();
