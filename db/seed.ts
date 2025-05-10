591cb735e918eec6e6cafad71384f8e6bae1c847
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2025-05-10 16:21:56 +1200

    bug fix

--- db/seed.ts
+++ db/seed.ts
@@ -2,16 +2,9 @@ import sampleData from "@/db/sample-data";
 import { prisma } from "@/db/prisma";
 
 const seed = async () => {
-  try {
-    await prisma.product.deleteMany();
-    await prisma.product.createMany({ data: sampleData.products });
-    console.log("Database seeded successfully");
-  } catch (error) {
-    console.error(`Error seeding database: ${error}`);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+  await prisma.product.deleteMany();
+  await prisma.product.createMany({ data: sampleData.products });
+  console.log("Database seeded successfully");
 };
 
 seed();
