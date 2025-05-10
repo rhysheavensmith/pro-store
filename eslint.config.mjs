36b83afb73fc6714f7856ed9352709c2a2ab75cd
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2025-05-10 17:38:03 +1200

    ignore prisma eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["**/prisma/lib/generated/**"],
+    ignores: ["**/prisma/**"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
