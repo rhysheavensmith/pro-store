2d2ffeeceab21f6447d7f5a4884bc25853702e59
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2025-05-14 18:43:57 +1200

    added auth screen and actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,77 @@
+"use server";
+
+// import { isRedirectError } from "next/dist/client/components/redirect";
+import { signIn, signOut } from "@/auth";
+import { signInFormSchema, signUpFormSchema } from "../validators";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
+
+// Sign in the user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    await signIn("credentials", user);
+
+    return { success: true, message: "Signed in successfully" };
+  } catch (error) {
+    // if (isRedirectError(error)) {
+    //   throw error;
+    // }
+    console.log(error);
+
+    return { success: false, message: "Invalid email or password" };
+  }
+}
+
+// Register a new user
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      confirmPassword: formData.get("confirmPassword"),
+      password: formData.get("password"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User created successfully" };
+  } catch (error) {
+    // if (isRedirectError(error)) {
+    //   throw error;
+    // }
+    console.log(error);
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+// Sign the user out
+export async function SignOutUser() {
+  await signOut();
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,25 @@ export function formatNumberWithDecimal(num: number): string {
     ? `${wholeNumber}.${decimalNumber.padEnd(2, "0")}`
     : `${wholeNumber}.00`;
 }
+
+// Format Errors
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any): string {
+  if (error.name === "ZodError") {
+    // Handle Zod validation errors
+    const fieldErrors = error.errors.map((e: any) => e.message);
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    // Handle unique constraint violations (e.g., email already exists)
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    // Fallback for all other error types
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
