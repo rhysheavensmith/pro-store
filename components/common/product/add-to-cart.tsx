f70367dce81044596abe0a510745e3d6a3cd6c97
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2025-05-22 19:12:33 +1200

    added the useTransition hook to set loading states

--- components/common/product/add-to-cart.tsx
+++ components/common/product/add-to-cart.tsx
@@ -1,64 +1,100 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { CartItem, Cart } from "@/types";
 import { redirect } from "next/navigation";
+import { useTransition } from "react";
 
 const AddToCart = ({ item, cart }: { item: CartItem; cart: Cart }) => {
+  const [isPending, startTransition] = useTransition();
+
   const redirectToCart = () => {
     redirect("/cart");
   };
 
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error(res.message);
-    }
+      if (!res.success) {
+        toast.error(res.message);
+      }
 
-    toast.success(`${res.message}`, {
-      action: {
-        label: "Go to cart",
-        onClick: redirectToCart,
-      },
+      toast.success(`${res.message}`, {
+        action: {
+          label: "Go to cart",
+          onClick: redirectToCart,
+        },
+      });
     });
   };
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    if (!res.success) {
-      toast.error(res.message);
-    }
+      if (!res.success) {
+        toast.error(res.message);
+      }
 
-    toast.success(`${res.message}`, {
-      action: {
-        label: "Go to cart",
-        onClick: redirectToCart,
-      },
+      toast.success(`${res.message}`, {
+        action: {
+          label: "Go to cart",
+          onClick: redirectToCart,
+        },
+      });
     });
   };
 
   const existItem =
-    cart && cart.items.find((x) => x.productId === item.productId);
+    cart && cart.items?.find((x) => x.productId === item.productId);
 
   return existItem ? (
     <div className="mx-auto flex items-center justify-center gap-2">
-      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="w-4 h-4" />
+      <Button
+        type="button"
+        variant="outline"
+        onClick={handleRemoveFromCart}
+        disabled={isPending}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
-      <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="w-4 h-4" />
+      <Button
+        type="button"
+        variant="outline"
+        onClick={handleAddToCart}
+        disabled={isPending}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
       </Button>
     </div>
   ) : (
-    <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus className="w-4 h-4" />
-      Add to cart
+    <Button
+      className="w-full"
+      type="button"
+      onClick={handleAddToCart}
+      disabled={isPending}
+    >
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <>
+          <Plus className="w-4 h-4" />
+          Add to cart
+        </>
+      )}
     </Button>
   );
 };
